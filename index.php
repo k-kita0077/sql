9f43da8d2e34fcc84579aeca4930c75689cf68d1
k_kita <ak2205d@icloud.com>
2020-06-07 19:08:00 +0900

    commit2

--- index.php
+++ index.php
@@ -19,6 +19,38 @@ for($i = 0;$i < 100;$i++) {
     $bb = "bb".$i;
     $cc = "cc".$i;
 
+    $ar = [$aa,$bb,$cc];
+    array_push($array,$ar);
+
+}
+
+var_dump($array);
+
+
+foreach($array as $value) {
+    $aa = $value[0];
+    $bb = $value[1];
+    $cc = $value[2];
+
+    $stmt->bindValue(":value1", $aa, PDO::PARAM_STR);
+    $stmt->bindValue(":value2", $bb, PDO::PARAM_STR);
+    $stmt->bindValue(":value3", $cc, PDO::PARAM_STR);
+
+    $status = $stmt->execute();
+
+    if($status==false){
+        $error = $stmt->errorInfo();
+        exit("SQLError:".$error[2]);
+    }else{
+    }
+}
+
+for($i = 0;$i < 100;$i++) {
+
+    $aa = "aa".$i;
+    $bb = "bb".$i;
+    $cc = "cc".$i;
+
     $stmt->bindValue(":value1", $aa, PDO::PARAM_STR);
     $stmt->bindValue(":value2", $bb, PDO::PARAM_STR);
     $stmt->bindValue(":value3", $cc, PDO::PARAM_STR);
