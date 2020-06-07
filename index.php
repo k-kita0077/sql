e3866579e6f531be00c2139a46e4618ca583eb78
k_kita <ak2205d@icloud.com>
2020-06-07 18:36:19 +0900

    commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+function db_conn() {
+    try{
+        $pdo = new PDO('mysql:dbname=test_table;charset=utf8;host=localhost','root','');
+    } catch (PDOException $e) {
+        exit('DbConnectError:'.$e->getMessage());
+    }
+    return $pdo;
+}
+
+$pdo = db_conn();
+$stmt = $pdo->prepare("INSERT INTO test_table(id,value1,value2,value3) VALUES(null,:value1,:value2,:value3)");
+$array = [];
+
+for($i = 0;$i < 100;$i++) {
+
+    $aa = "aa".$i;
+    $bb = "bb".$i;
+    $cc = "cc".$i;
+
+    $stmt->bindValue(":value1", $aa, PDO::PARAM_STR);
+    $stmt->bindValue(":value2", $bb, PDO::PARAM_STR);
+    $stmt->bindValue(":value3", $cc, PDO::PARAM_STR);
+
+    $status = $stmt->execute();
+
+    if($status==false){
+        $error = $stmt->errorInfo();
+        exit("SQLError:".$error[2]);
+    }else{
+    }
+    
+}
+
+
+
+//var_dump($array);
+?>
